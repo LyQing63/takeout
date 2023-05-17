608d5f82d92f096f25dd09681524759892c40da8
yourusername <youremail@example.com>
2023-05-17 23:31:44 +0800

    优化异常处理器魔法值

--- src/main/java/org/LyQing/takeout/common/GlobalExceptionHandler.java
+++ src/main/java/org/LyQing/takeout/common/GlobalExceptionHandler.java
@@ -1,2 +1,42 @@
-package org.LyQing.takeout.common;public class GlobalExceptionHandler {
+package org.LyQing.takeout.common;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
+/**
+ * @author yjxx_2022
+ */
+@ControllerAdvice(annotations = {RestController.class, Controller.class})
+@ResponseBody
+@Slf4j
+public class GlobalExceptionHandler {
+
+    private static final String DUPLICATE_EXCEPTION = "Duplicate entry";
+
+    /**
+     * 异常处理方法
+     * @param ex
+     * @return R
+     */
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public R<String> exceptionHandler(SQLIntegrityConstraintViolationException ex) {
+        String exMessage = ex.getMessage();
+        log.error(exMessage);
+
+        if (exMessage.contains(DUPLICATE_EXCEPTION)) {
+            String[] split = exMessage.split(" ");
+            String msg = split[2] + "已存在";
+
+            return R.error(msg);
+        }
+
+        return R.error("未知错误");
+    }
+
 }
