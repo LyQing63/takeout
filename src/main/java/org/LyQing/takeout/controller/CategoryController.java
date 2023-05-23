5bda187ccacd2df0f31483da13e81907f474f782
yourusername <youremail@example.com>
2023-05-23 19:44:48 +0800

    实现添加菜品中菜品分类下拉框中数据的查询与提交

--- src/main/java/org/LyQing/takeout/controller/CategoryController.java
+++ src/main/java/org/LyQing/takeout/controller/CategoryController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 /**
  * 类别控制器
@@ -66,7 +67,6 @@ public class CategoryController {
     public R<String> delete(Long ids) {
         log.info("删除分类，id为:{}", ids);
 
-//        categoryService.removeById(ids);
         categoryService.remove(ids);
 
         return R.success("分类信息删除成功");
@@ -86,6 +86,26 @@ public class CategoryController {
         categoryService.updateById(category);
 
         return R.success("修改分类信息成功");
+    }
+
+    /**
+     * 根据条件查询分类数据
+     *
+     * @param category 类别
+     * @return {@link R}<{@link List}<{@link Category}>>
+     */
+    @GetMapping("/list")
+    public R<List<Category>> list(Category category) {
+        //条件构造器
+        LambdaQueryWrapper<Category> queryWrapper = new LambdaQueryWrapper<>();
+        //添加条件
+        queryWrapper.eq(category.getType() != null, Category::getType, category.getType());
+        //排序条件
+        queryWrapper.orderByAsc(Category::getSort).orderByAsc(Category::getUpdateTime);
+
+        List<Category> list = categoryService.list(queryWrapper);
+
+        return R.success(list);
     }
 
 }
--- src/main/java/org/LyQing/takeout/service/impl/CategoryServiceImpl.java
+++ src/main/java/org/LyQing/takeout/service/impl/CategoryServiceImpl.java
@@ -65,4 +65,6 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryMapper, Category> i
 
     }
 
+
+
 }
